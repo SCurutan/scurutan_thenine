fff210a6f4f00f234c398e85b48581c375e51dd0
SCurutan <sabrina.curutan@georgebrown.ca>
2021-03-22 17:56:43 -0400

    5th comm - save before adding featherlight plug-in

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 function start(){
     $("#menu a:first").click();
-
 }
 
 $(window).on("load", start);
+
 
 function loadpage(e){
 
@@ -11,12 +11,24 @@ function loadpage(e){
     
     // $("#menu a.active").removeClass("active");
     // $(this).addClass("active");
-
     //ADD ACTIVE CLASS ICONS FOR HOME PAGE ICONS//
 
     var href = $(this).attr("href");    
     $("#content").load(href);
 
+    $("#title").removeClass("hidden");
+    $("#header-logo").removeClass("hidden");
 }
 
 $(document).on("click", "#menu a", loadpage);
+
+
+function loadleagues(e){
+
+    e.preventDefault();
+
+    $("#title").addClass("hidden");
+    $("#header-logo").addClass("hidden");
+}
+
+$(document).on("click", "#menu a:last", loadleagues);
